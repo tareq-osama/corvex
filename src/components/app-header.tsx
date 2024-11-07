e8f3e842252a5f6f5625af32f0c9973f66ecc7a6
tareq-osama <tarekaless99@gmail.com>
2024-11-07 05:43:43 +0300

    borders-cleared

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -20,7 +20,7 @@ interface AppHeaderProps {
 
 export function AppHeader({ currentPage, parentPage }: AppHeaderProps) {
   return (
-    <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+    <header className="flex h-16 shrink-0 items-center gap-2  px-4 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
       <div className="flex items-center gap-2">
         <SidebarTrigger className="-ml-1" />
         <Separator orientation="vertical" className="mr-2 h-4" />
